fe128b38da79542c0db7554a411d29517b010930
Lourdes947 <110195021+Lourdes947@users.noreply.github.com>
2022-12-26 14:35:25 -0300

    Back End

---
+++ src/main/java/com/portfolio/mgb/Interface/IPersonaService.java
@@ -0,0 +1,19 @@
+package com.portfolio.mgb.Interface;
+
+import com.portfolio.mgb.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    //traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //buscar una persona por ID
+     public Persona findPersona(Long id);
+}
