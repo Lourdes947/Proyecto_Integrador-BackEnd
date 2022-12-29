69de272ddd6ed861fb0c96ffe0437ccab0079dfe
Lourdes947 <110195021+Lourdes947@users.noreply.github.com>
2022-12-29 16:48:36 -0300

    Actualizando

--- src/main/java/com/portfolio/mgb/Entity/hys.java
+++ src/main/java/com/portfolio/mgb/Entity/hys.java
@@ -1,10 +1,12 @@
 
 package com.portfolio.mgb.Entity;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Entity
 public class hys {
